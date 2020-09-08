121dfd388fb256e7ecdfbc563420e00f3b8d081d
georgemihaila <georgemihaila20@yahoo.com>
2020-09-07 19:11:14 -0500

    built with twine

---
+++ build/lib/ml_things/ml_things.py
@@ -0,0 +1,10 @@
+def convert(my_name):
+    """
+    Print a line about converting a notebook.
+    Args:
+        my_name (str): person's name
+    Returns:
+        None
+    """
+
+    print(f"I'll convert a notebook for you some day, {my_name}.")
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+setup(
+    name="notebookc",
+    version="0.0.1",
+    author="Jeff Hale",
+    author_email="jeffmshale@gmail.com",
+    description="A package to convert your Jupyter Notebook",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/your_package/homepage/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
