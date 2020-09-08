e7510f1a7e94b448c22c5d86d50ed896abfe8f37
georgemihaila <georgemihaila20@yahoo.com>
2020-09-08 16:21:05 -0500

    test alternative nameing

--- src/ml_things/__init__.py
+++ src/ml_things/__init__.py
@@ -4,6 +4,9 @@
 
 __version__ = "0.0.1"
 
-# functions
+# functions imports
 from .array_functions import (padding,
                               batching)
+
+# alternative names
+from .array_functions import batching as chunking
