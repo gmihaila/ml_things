4a1dd78217c0fac75f3bd599cd896ca3ee89d6ec
georgemihaila <georgemihaila20@yahoo.com>
2020-09-29 20:18:26 -0500

    plot_dict added in init()

--- src/ml_things/__init__.py
+++ src/ml_things/__init__.py
@@ -9,6 +9,7 @@ from .array_functions import (pad_array,
                               batch_array)
 from .web_related import (download_from)
 from .plot_functions import (plot_array,
+                             plot_dict,
                              plot_confusion_matrix)
 from .text_functions import (clean_text)
 # installed ftfy to fix any UNICODE problems in text data
