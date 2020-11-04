a0e49d8dff7b7b63275c89cd9810b11b14e51cc3
georgemihaila <georgemihaila20@yahoo.com>
2020-11-04 09:16:01 -0600

    download_from updated to check url is valid

--- src/ml_things/web_related.py
+++ src/ml_things/web_related.py
@@ -16,11 +16,12 @@
 
 import os
 import requests
+import warnings
 
 
 def download_from(url, path):
     r"""
-    Download file from url.
+    Download file from url. If invalid URL is detected it will print message and it will return empty string for file downloaded path.
 
     Arguments:
 
@@ -34,20 +35,40 @@ def download_from(url, path):
 
         :obj:`str`: Path where file was saved.
 
+    Note:
+
+        It will not work with .zip urls from Dropbox. Need to fix it!
+
     """
 
     # get file name from url
     file_name = os.path.basename(url)
+    if file_name[-4:] == '.zip':
+        # Warning - it mights not download the actual zip. It will not work with Dropbox.
+        warnings.warn(f'File name {file_name} is a zip. It might not download it properly!',
+                      UserWarning)
+
     # create directory frm path if it doesn't exist
     os.makedirs(path) if not os.path.isdir(path) else None
     # find path where file will be saved
     file_path = os.path.join(path, file_name)
+
+    # if files does not exist - download
+    response = requests.get(url)
+    # Chekc if url is valid or not.
+    if response.status_code != 200:
+        # Invalid or not responsive url.
+        print(f'Invalid url! Return code: {response.status_code}')
+        # Return empty string for file_path
+
+        return ''
+
     # check if file already exists
-    if not os.path.isfile(file_path):
-        # if files does not exist - download
-        response = requests.get(url)
+    elif not os.path.isfile(file_path):
+        # Download file.
         file_size = open(file_path, 'wb').write(response.content)
         file_size = '{0:.2f}MB'.format(file_size / 1024)
         # print file details
         print("%s %s" % (file_path, file_size))
+
     return file_path
